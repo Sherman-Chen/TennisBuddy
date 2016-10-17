462595295b357529a93db1f9035a0b5e995d427e
Sherman <shc410@gmail.com>
2016-10-17 15:04:03 -0700

    set up some basic routing in express server

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 'use strict'
 
+var port = process.env.PORT || 8888;
+
 var express = require('express');
 var app = express();
-
-// app.use('/', express.static('public')); eventually...
 
 app.get('/', function(req, res) {
   res.send('Under Construction');
 });
 
-app.listen('8888', function() {
-  console.log('app is live on port 8888');
+app.use('/landing', express.static('public'));
+
+app.get('/landing', function(req, res) {
+  res.sendfile('public/index.html', {root: __dirname }, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('landing served');
+    }
+  });
+});
+
+// start server
+app.listen(port, function() {
+  console.log('app is live on port ' + port);
 });
