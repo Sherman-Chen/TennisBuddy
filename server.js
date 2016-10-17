0fa1fd294bfd1d4b9f39b5d43f29dc0b217997ae
Sherman <shc410@gmail.com>
2016-10-17 11:59:58 -0700

    get server running

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 'use strict'
 
 var express = require('express');
+var app = express();
+
+// app.use('/', express.static('public')); eventually...
+
+app.get('/', function(req, res) {
+  res.send('Under Construction');
+});
+
+app.listen('8888', function() {
+  console.log('app is live on port 8888');
+
+});
