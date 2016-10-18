7cfb28befa13d2fd36353cc50655ebd634801731
Sherman <shc410@gmail.com>
2016-10-18 02:05:25 -0700

    slight refactor of server, added comments and future goals

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ mongoose.connect('mongodb://localhost/tennisbuddy');
 var express = require('express');
 var app = express();
 
+// serve angular client
 app.use('/', express.static('client'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-// default user schema
+// default user schema, users will have a username, a pw, and an optional buddies list in form of array
 var UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -31,6 +32,7 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
+// endpoint for users database
 app.get('/api/users', function(req, res) {
   // UserSchema.find(function(err, user) {
   //   if (err) {
@@ -41,10 +43,12 @@ app.get('/api/users', function(req, res) {
   res.json({users: mock});
 });
 
+// endpoint to post to users database
 app.post('/api/users', function(req, res) {
   // post to DB with new user
 });
 
+// endpoint for SF tennis court database
 app.get('/api/courts', function(req, res) {
   res.json({courts: sfCourts});
 });
@@ -53,3 +57,8 @@ app.get('/api/courts', function(req, res) {
 app.listen(port, function() {
   console.log('app is live on port ' + port);
 });
+
+
+
+// things to do
+// modularize express server, and also create routing
