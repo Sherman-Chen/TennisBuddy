f659236cae115abfc7947c51674f1c5b6e40986d
Sherman <shc410@gmail.com>
2016-10-18 22:07:26 -0700

    minor changes for presentation

--- client/app/signin/signin.js
+++ client/app/signin/signin.js
@@ -2,11 +2,14 @@ angular.module('TennisBuddy.signin', [])
 
 .controller('signinCtrl', function($scope, Users) {
   $scope.users = {};
+  $scope.dummy;
 
   Users.getUsers().then(function(user) {
     $scope.users.users = user;
+    $scope.dummy = $scope.users.users[4].buddies;
   });
   
+
   $scope.btnclick = function() {
     console.log('wired');
     // Users.getUsers().then(function(user) {
